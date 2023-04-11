85065769d555a39b7975026ea4d985de8c61c296
HuyHoang307 <doantienhuyhoang@gmail.com>
2023-04-11 17:10:08 +0700

    2390. Removing Stars From a String

---
+++ 2390.removing-stars-from-a-string/2390.removing-stars-from-a-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        res = []
+        for i in s:
+            if i == "*":
+                res.pop()
+            else:
+                res.append(i)
+        return "".join(res)
+    
+
+s = "erase*****"
+solution = Solution()
+print(solution.removeStars(s))
